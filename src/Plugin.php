620121467e9650bcf6c4c57b9c4fafd890a1f646
Joe Huss <detain@interserver.net>
2017-06-24 15:10:10 -0400

    a bunch of updates improving the code making use of self:: and other imprvoements

--- src/Plugin.php
+++ src/Plugin.php
@@ -26,7 +26,7 @@ class Plugin {
 	public static function getActivate(GenericEvent $event) {
 		$license = $event->getSubject();
 		if ($event['category'] == SERVICE_TYPES_FANTASTICO) {
-			myadmin_log('licenses', 'info', 'Lxc Activation', __LINE__, __FILE__);
+			myadmin_log(self::$module, 'info', 'Lxc Activation', __LINE__, __FILE__);
 			function_requirements('activate_lxc');
 			activate_lxc($license->get_ip(), $event['field1']);
 			$event->stopPropagation();
@@ -36,12 +36,12 @@ class Plugin {
 	public static function getChangeIp(GenericEvent $event) {
 		if ($event['category'] == SERVICE_TYPES_FANTASTICO) {
 			$license = $event->getSubject();
-			$settings = get_module_settings('licenses');
+			$settings = get_module_settings(self::$module);
 			$lxc = new Lxc(FANTASTICO_USERNAME, FANTASTICO_PASSWORD);
-			myadmin_log('licenses', 'info', "IP Change - (OLD:".$license->get_ip().") (NEW:{$event['newip']})", __LINE__, __FILE__);
+			myadmin_log(self::$module, 'info', "IP Change - (OLD:".$license->get_ip().") (NEW:{$event['newip']})", __LINE__, __FILE__);
 			$result = $lxc->editIp($license->get_ip(), $event['newip']);
 			if (isset($result['faultcode'])) {
-				myadmin_log('licenses', 'error', 'Lxc editIp('.$license->get_ip().', '.$event['newip'].') returned Fault '.$result['faultcode'].': '.$result['fault'], __LINE__, __FILE__);
+				myadmin_log(self::$module, 'error', 'Lxc editIp('.$license->get_ip().', '.$event['newip'].') returned Fault '.$result['faultcode'].': '.$result['fault'], __LINE__, __FILE__);
 				$event['status'] = 'error';
 				$event['status_text'] = 'Error Code '.$result['faultcode'].': '.$result['fault'];
 			} else {
@@ -56,11 +56,10 @@ class Plugin {
 
 	public static function getMenu(GenericEvent $event) {
 		$menu = $event->getSubject();
-		$module = 'licenses';
 		if ($GLOBALS['tf']->ima == 'admin') {
-			$menu->add_link($module, 'choice=none.reusable_lxc', 'icons/database_warning_48.png', 'ReUsable Lxc Licenses');
-			$menu->add_link($module, 'choice=none.lxc_list', 'icons/database_warning_48.png', 'Lxc Licenses Breakdown');
-			$menu->add_link($module.'api', 'choice=none.lxc_licenses_list', 'whm/createacct.gif', 'List all Lxc Licenses');
+			$menu->add_link(self::$module, 'choice=none.reusable_lxc', 'icons/database_warning_48.png', 'ReUsable Lxc Licenses');
+			$menu->add_link(self::$module, 'choice=none.lxc_list', 'icons/database_warning_48.png', 'Lxc Licenses Breakdown');
+			$menu->add_link(self::$module.'api', 'choice=none.lxc_licenses_list', 'whm/createacct.gif', 'List all Lxc Licenses');
 		}
 	}
 
@@ -81,11 +80,10 @@ class Plugin {
 	}
 
 	public static function getSettings(GenericEvent $event) {
-		$module = 'vps';
 		$settings = $event->getSubject();
-		$settings->add_text_setting($module, 'Slice Costs', 'vps_slice_lxc_cost', 'LXC VPS Cost Per Slice:', 'LXC VPS will cost this much for 1 slice.', $settings->get_setting('VPS_SLICE_LXC_COST'));
-		//$settings->add_select_master($module, 'Default Servers', $module, 'new_vps_lxc_server', 'LXC NJ Server', NEW_VPS_LXC_SERVER, 9, 1);
-		$settings->add_dropdown_setting($module, 'Out of Stock', 'outofstock_lxc', 'Out Of Stock LXC Secaucus', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_LXC'), array('0', '1'), array('No', 'Yes',));
+		$settings->add_text_setting(self::$module, 'Slice Costs', 'vps_slice_lxc_cost', 'LXC VPS Cost Per Slice:', 'LXC VPS will cost this much for 1 slice.', $settings->get_setting('VPS_SLICE_LXC_COST'));
+		//$settings->add_select_master(self::$module, 'Default Servers', $module, 'new_vps_lxc_server', 'LXC NJ Server', NEW_VPS_LXC_SERVER, 9, 1);
+		$settings->add_dropdown_setting(self::$module, 'Out of Stock', 'outofstock_lxc', 'Out Of Stock LXC Secaucus', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_LXC'), array('0', '1'), array('No', 'Yes',));
 	}
 
 }
