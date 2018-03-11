b6c92c5b403dc26c0e7374b49e984071188d5be7
Joe Huss <detain@interserver.net>
2018-03-10 22:40:11 -0500

    updates

--- src/Plugin.php
+++ src/Plugin.php
@@ -87,21 +87,21 @@ class Plugin {
 		}
 	}
 
-        public static function GetList($name = '') {
-            $exp = explode("\n",shell_exec('lxc list '.$name));
-            $res = [];
-            foreach ($exp as $k => $v)
-                if ($k % 2 && $k!=1 && $v!='') {
-                    $exp2 = explode('|',$v);
-                    $exp3 = explode(' (',trim($exp2[3]));
-                    $res[] = [
-        	            'name' => trim($exp2[1]), 
-	                    'status' => strtolower(trim($exp2[2])), 
-                	    'ipv4' => $exp3[0], 
-        	            'card' => substr($exp3[1],0,strlen($exp3[1])-1),
-	                    'ipv6' => trim($exp2[4])
-                    ];
-                }
-            return $res;
-        }
+		public static function GetList($name = '') {
+			$exp = explode("\n",shell_exec('lxc list '.$name));
+			$res = [];
+			foreach ($exp as $k => $v)
+				if ($k % 2 && $k!=1 && $v!='') {
+					$exp2 = explode('|',$v);
+					$exp3 = explode(' (',trim($exp2[3]));
+					$res[] = [
+						'name' => trim($exp2[1]), 
+						'status' => strtolower(trim($exp2[2])), 
+						'ipv4' => $exp3[0], 
+						'card' => substr($exp3[1],0,strlen($exp3[1])-1),
+						'ipv6' => trim($exp2[4])
+					];
+				}
+			return $res;
+		}
 }
